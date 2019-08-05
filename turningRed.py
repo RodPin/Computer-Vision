bdb71fc9cccc8b9809ae3c932aea6bb4481d8717
RODRIGO <prodrigo@poli.ufrj.br>
2019-08-05 15:52:30 -0300

    first commit

---
+++ turningRed.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+from PIL import Image
+
+img = Image.open("Imagens/flower.jpg")
+
+##creating the matrix of the image
+img_mat = np.array(img)
+
+##checking if it is an matrix
+type(img_mat)
+
+##showing the ploted img
+plt.imshow(img_mat)
+
+img_red = img_mat.copy()
+##removing blue colors from all pixels   ":"  means every pixel
+img_red[:,:,2] = 0
+##removing green
+img_red[:,:,1] = 0
+
+
+##showing red_img
+plt.imshow(img_red)
